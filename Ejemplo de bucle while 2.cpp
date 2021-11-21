1a19edf1c466a6fae6e5fa012267fa91dd01a701
Maria Castro <92470095+Mariacastro28@users.noreply.github.com>
2021-11-21 00:23:44 -0400

    Add files via upload

---
+++ Ejemplo de bucle while 2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {    
+
+int contador =1;
+  
+while (contador < 10) 
+{
+cout<<("% d\n",contador);
+contador++;
+}
+
+return 0;
+}
